185918c7b1e5ab9883d4efcbca177e5a9ea26834
Aizen09 <scilentgamer99@gmail.com>
2025-02-21 14:45:14 +0530

    Simple server in MongoDB Atlas

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const { resolve } = require('path');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = 3010;
+
+app.use(express.static('static'));
+const db = async() => {
+  try{
+    await mongoose.connect('mongodb+srv://mohitodomain:88347317@cluster0.i3hea.mongodb.net/Clusters?retryWrites=true&w=majority&appName=Cluster0')
+    .then (res => console.log("DB connected"))
+    .catch (e => console.log("Unexpected error occured"))
+  }
+  catch(e){
+    console.log(e);
+  }
+}
+
+app.get('/', (req, res) => {
+  res.sendFile(resolve(__dirname, 'pages/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
